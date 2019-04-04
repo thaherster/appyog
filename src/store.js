46919858a5e118e55b574723c36b0822209f1b8f
thaherster <thaherster@gmail.com>
2019-04-04 16:22:06 +0530

    REDUX DEVTOOL

--- src/store.js
+++ src/store.js
@@ -5,13 +5,22 @@ import rootReducer from "./reducers"; //since we called out root reducer index.j
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+if (window.navigator.userAgent.includes("Chrome")) {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
